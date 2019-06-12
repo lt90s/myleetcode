7119d75c9f74a31febd776c8adc05942dec25441
lt90s <liangtao90s@163.com>
2019-06-12 22:42:36 +0700

    3. Longest Substring Without Repeating Characters O(n)

--- p3/main.go
+++ p3/main.go
@@ -22,8 +22,29 @@ func lengthOfLongestSubstring(s string) int {
 	return maxLen
 }
 
+func lengthOfLongestSubstring_On(s string) int {
+	indexes := [256]int{}
+	rs, cursor := 0, 0
+	for i := 0; i < len(s); i++ {
+		// repeat
+		if indexes[s[i]] != 0 && indexes[s[i]] >= cursor {
+			cursor = indexes[s[i]]
+		}
+		indexes[s[i]] = i + 1
+		if i - cursor + 1 > rs {
+			rs = i - cursor + 1
+		}
+	}
+	return rs
+}
+
 func main() {
 	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
 	fmt.Println(lengthOfLongestSubstring("bbbbb"))
 	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+
+	fmt.Println(lengthOfLongestSubstring_On("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring_On("bbbbb"))
+	fmt.Println(lengthOfLongestSubstring_On("pwwkew"))
+	fmt.Println(lengthOfLongestSubstring_On(" "))
 }
